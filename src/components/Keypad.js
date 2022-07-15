02e3e6594fe4e1dffafecc2564e2e3a75a4e7047
kamausuzan197 <kamaususan197@gmail.com>
2022-07-15 06:27:52 +0300

    working with events

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,9 @@
 // Code Keypad Component Here
+
+function keypad(){
+    function handleChange(event) {
+        console.log(event.target.value);
+    return(
+        <input onChange={()=>console.log ('Entering password...')}type='password'></input>
+    )
+}
